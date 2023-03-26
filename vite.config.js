850a2774de1291f1f596d6b1396ea0a75fe3e275
Mark Croxton <mcroxton@hallmark-design.co.uk>
2023-03-26 23:36:39 +0100

    Add Stylelint

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import legacy from '@vitejs/plugin-legacy';
 import ViteRestart from 'vite-plugin-restart';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import vue from '@vitejs/plugin-vue'
+import stylelint from 'vite-plugin-stylelint';
 
 // https://vitejs.dev/config/
 export default ({ command }) => ({
@@ -27,6 +28,9 @@ export default ({ command }) => ({
         }),
         viteEslintPlugin({
             cache: false,
+            fix: true,
+        }),
+        stylelint({
             fix: true,
         }),
         ViteRestart({
