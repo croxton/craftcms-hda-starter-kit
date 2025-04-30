bad240d7955e08242af18dab8fd3c439e3eff9fc
Mark Croxton <mcroxton@hallmark-design.co.uk>
2025-04-30 16:20:48 +0100

    Remove trailing slash from PRIMARY_SITE_URL on install.

--- .ddev/commands/web/set_primary_site_url.sh
+++ .ddev/commands/web/set_primary_site_url.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-## Description: Sets the default PRIMARY_SITE_URL of your Craft .env file to your ddev project name.
+## Description: Sets the default PRIMARY_SITE_URL of your Craft .env file to
+## your ddev project name, and removes the trailing slash from the ddev URL.
 ## Usage: set_primary_site_url
 ## Example: "ddev set_primary_site_url"
 
-sed -i -s "s/YOUR-CRAFT-PROJECT-NAME/${DDEV_SITENAME}/" /var/www/html/.env
+sed -i -s "s#YOUR-CRAFT-PROJECT-NAME#${DDEV_SITENAME}#" /var/www/html/.env
+sed -i -s "s#ddev.site/#ddev.site#" /var/www/html/.env
